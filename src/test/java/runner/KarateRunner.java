eb13dde089a509da7ae7abecd5013b69b3b291c0
paollafusco <paollacordeirof@gmail.com>
2024-07-23 20:41:30 +0100

    Adding reporting and running in parallel.

--- src/test/java/runner/KarateRunner.java
+++ src/test/java/runner/KarateRunner.java
@@ -1,11 +1,18 @@
 package runner;
 
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class KarateRunner {
 
-    @Karate.Test
-    Karate testAll() {
-        return (Karate) Karate.run("classpath:features").tags("~@bug","~@ignore");
+    @BeforeAll
+    public static void setup() {
+        System.setProperty("karate.report.dir", "target/karate-reports");
+    }
+
+    @Test
+    public void testAll() {
+        Karate.run("classpath:features").tags("~@bug", "~@ignore").parallel(6);
     }
 }
--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,11 +1,6 @@
 function fn() {
-
-  var config = {
-    baseUrl: "https://restful-booker.herokuapp.com/"
+  karate.configure('report', { showLog: true, showAllSteps: true });
+  return {
+    baseUrl: 'https://restful-booker.herokuapp.com/'
   };
-
-  karate.log('karate-config.js is executed');
-  karate.log('Base URL:', config.baseUrl);
-
-  return config;
 }
